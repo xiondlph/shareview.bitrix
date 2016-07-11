dde03fb80f8d503aaf78714688ac8cfa6338910d
Ismailov Shuhrat <admin@ismax.ru>
2016-07-11 18:35:06 +0300

    Доп. файлы

---
+++ shareview/install/components/ismax/shareview/.description.php
@@ -0,0 +1,9 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+    "NAME" 			=> GetMessage("SHAREVIEW_COMPONENT"),
+    "DESCRIPTION" 	=> GetMessage("SHAREVIEW_COMPONENT_DESCRIPTION"),
+    "ICON" 			=> "/images/shareview.gif"
+);
+?>
