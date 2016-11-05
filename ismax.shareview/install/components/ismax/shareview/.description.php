1b4a316060f7e6a1f48b1cd3eef7aa313aa5804a
xiondlph <admin@ismax.ru>
2016-11-05 14:49:02 +0300

    Переименование файлов логотипов

--- ismax.shareview/install/components/ismax/shareview/.description.php
+++ ismax.shareview/install/components/ismax/shareview/.description.php
@@ -4,6 +4,6 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 $arComponentDescription = array(
 	"NAME"			=> GetMessage("SHAREVIEW_COMPONENT"),
 	"DESCRIPTION"	=> GetMessage("SHAREVIEW_COMPONENT_DESCRIPTION"),
-	"ICON"			=> "/images/shareview.gif"
+	"ICON"			=> "/images/logo32x32.gif"
 );
 ?>
